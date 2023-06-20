a26ae24e66e8fb98518c7934a5345ccad6d50df4
i小昕 <1571466236@qq.com>
2023-06-20 21:59:12 +0800

    首页数据显示模块创建

---
+++ store-service/service-home/src/main/java/io/xccit/store/service/IHomeService.java
@@ -0,0 +1,9 @@
+package io.xccit.store.service;
+
+/**
+ * @author CH_ywx
+ * @date 2023-06-20
+ * @description
+ */
+public interface IHomeService {
+}
---
+++ store-service/service-home/src/main/java/io/xccit/store/service/impl/HomeServiceImpl.java
@@ -0,0 +1,9 @@
+package io.xccit.store.service.impl;
+
+/**
+ * @author CH_ywx
+ * @date 2023-06-20
+ * @description
+ */
+public class HomeServiceImpl {
+}
